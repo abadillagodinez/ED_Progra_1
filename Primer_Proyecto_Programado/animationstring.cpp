582e2e17f322bbd17b72af562d63a72a44e2ad62
Unknown <adriangazubg7@gmail.com>
2018-10-10 03:51:02 -0600

    Avance de interfaz
    Falta las comparaciones de operadores sobrecargados

---
+++ Primer_Proyecto_Programado/animationstring.cpp
@@ -0,0 +1,6 @@
+#include "animationstring.h"
+
+AnimationString::AnimationString()
+{
+
+}
---
+++ Primer_Proyecto_Programado/animationstring.h
@@ -0,0 +1,11 @@
+#ifndef ANIMATIONSTRING_H
+#define ANIMATIONSTRING_H
+
+
+class AnimationString
+{
+public:
+    AnimationString();
+};
+
+#endif // ANIMATIONSTRING_H
